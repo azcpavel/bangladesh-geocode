471862309f23ca8e8b1ee820762d8ab810016db2
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2020-01-24 12:28:21 +0600

    Id Update

--- src/Database/migrations/2019_01_14_145805_create_divisions_table.php
+++ src/Database/migrations/2019_01_14_145805_create_divisions_table.php
@@ -26,9 +26,9 @@ class CreateDivisionsTable extends Migration
 		});
 
 		DB::table('divisions')->insert([
-			['id' =>1, 'name' =>'Barishal', 'bn_name' =>'বরিশাল'],
+			['id' =>1, 'name' =>'Dhaka', 'bn_name' =>'ঢাকা'],			
 			['id' =>2, 'name' =>'Chattogram', 'bn_name' =>'চট্টগ্রাম'],
-			['id' =>3, 'name' =>'Dhaka', 'bn_name' =>'ঢাকা'],
+			['id' =>3, 'name' =>'Barishal', 'bn_name' =>'বরিশাল'],
 			['id' =>4, 'name' =>'Khulna', 'bn_name' =>'খুলনা'],
 			['id' =>5, 'name' =>'Rajshahi', 'bn_name' =>'রাজশাহী'],
 			['id' =>6, 'name' =>'Rangpur', 'bn_name' =>'রংপুর'],
